cd7ba68d9cceff87de30caebdfae2b0e79eb57f6
csengebalogh <csenge.balogh1214@gmail.com>
2022-03-21 18:19:58 +0100

    user and computer decisions are functional and displayed

--- app.js
+++ app.js
@@ -1,23 +1,41 @@
-const decisionDiv = document.querySelector('.decision')
+const cl = console.log
+const userDiv = document.querySelector('.user')
+const userDecision = document.getElementById('userDecision')
 const decisionDisplay = document.querySelector('#decisionDisplay')
+const botDecisionDiv = document.querySelector('#botDecision')
 const options = document.querySelectorAll('.option')
 const opts = Array.from(options)
+const botOptions = ["Rock", "Paper", "Scissors"]
 
 decisionDisplay.style.display = 'none'
 
+
+function botDecides () {
+    var botDecision = botOptions[
+        Math.floor(
+            Math.random() * botOptions.length
+        )
+    ]
+
+    botDecisionDiv.textContent = botDecision
+}
+
+function decisionMade (e) {
+    cl(e.target.id)
+    userDecision.textContent = e.target.id
+    console.log('decision made')
+    console.log(opts)
+    userDiv.style.display = 'none'
+    decisionDisplay.style.display = 'block'
+
+    botDecides()
+}
 
 for (var i=0;i<options.length;i++) {
     opts[i].addEventListener('click', decisionMade)
 }
 
-function decisionMade () {
-    console.log('decision made')
-    console.log(opts)
-    decisionDiv.style.display = 'none'
-    decisionDisplay.style.display = 'block'
-}
-
 console.log(options) // NodeList
 console.log(opts) // List
 
-
+cl(botDecision)
