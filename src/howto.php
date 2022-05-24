f0d1034d38546f82d780f5359fd7115a30ef0279
csengebalogh <csenge.balogh1214@gmail.com>
2022-05-24 20:40:49 +0200

    PHP renders pages

---
+++ src/howto.php
@@ -0,0 +1,7 @@
+<?php
+
+echo file_get_contents('templates/header.html');
+echo file_get_contents('templates/howto.html');
+echo file_get_contents('templates/footer.html');
+
+?>
---
+++ src/index.php
@@ -0,0 +1,7 @@
+<?php
+
+echo file_get_contents('templates/header.html');
+echo file_get_contents('templates/index.html');
+echo file_get_contents('templates/footer.html');
+
+?>
